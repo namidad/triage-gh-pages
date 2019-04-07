447b5618b1acf49742c836372bb6c662a4b8e6c4
namidad <salamonoliwier@gmail.com>
2019-04-07 16:29:29 +0200

    repaired error

--- src/features/nav/Menus/SignedInMenu.jsx
+++ src/features/nav/Menus/SignedInMenu.jsx
@@ -3,13 +3,11 @@ import { Menu, Dropdown } from 'semantic-ui-react'
 
 const SignedInMenu = ({signOut}) => {
 
-  this.handleClick = () => {
-    console.log("Witam");
-  }
+
 
     return(
             <Menu.Item position="right">
-            <Dropdown.Item onClick={this.handleClick} text="Przelacz" icon="download" />
+            <Dropdown.Item text="Przelacz" icon="download" />
             <Dropdown.Item onClick={signOut} text="Sign Out" icon="power" />
             </Menu.Item>
     )
